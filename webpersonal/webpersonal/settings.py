130fa6677320cdb3aa94aa5bd269e406fb361c8b
JuanMarchant <juanmarchantdev@gmail.com>
2022-01-11 20:10:10 -0300

    add whitenoise/staticfile-root

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # MEDIA FILES
 
