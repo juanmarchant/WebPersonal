c2f185fdbeabeee6e42995cf5f067f6ae3d66d76
JuanMarchant <juanmarchantdev@gmail.com>
2022-01-11 20:53:26 -0300

    change host

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default='False', cast=bool)
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['portfolio-juanmarchant.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
