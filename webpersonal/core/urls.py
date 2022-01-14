b705d4c77d388b4bb5d1e8d00e54689107c29e53
JuanMarchant <juanmarchantdev@gmail.com>
2022-01-13 22:54:44 -0300

    add robot.txt

--- webpersonal/core/urls.py
+++ webpersonal/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls.conf import path
 from .views import HomePageView
+from django.views.generic import TemplateView
 
 app_name='main'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('robots.txt', TemplateView.as_view(template_name='core/robots.txt', content_type='text/plain')),
 ]
